745c7061b9706fcc7a51ac88919b5c9f39e35165
stio <dev@stio.studio>
2023-10-20 16:17:03 +0200

    save

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-let content = document.querySelector(".content")
-
-let div = document.createElement("div")
-let contentBox = [div.cloneNode(true)]
-contentBox[0].classList.add("bigBox", "box-shadow")
-contentBox[0].style.display = "flex"
-content.append(contentBox[0])
-let box = div.cloneNode(true)
-box.classList.add("box")
-
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
-contentBox[0].append(box.cloneNode(true))
+// let rem = await(await fetch("/assets/projects.json")).json()
+let rem = await(await fetch("./projects.json")).json()
+contentCreate(rem)
+await translationSetup()
